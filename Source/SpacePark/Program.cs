a5bc38fdfed5341906cc0c53fce23fd50610d585
Fredrika Eriksson <56867257+erifredrika@users.noreply.github.com>
2020-03-20 12:16:22 +0100

    Added if/else and cool header
    Added some logic, perhaps temporary. We could of course do it more neatly I guess.
    
    Also added a super cool header yo

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -11,29 +11,74 @@ namespace SpacePark
         
         static async Task Main(string[] args)
         {
+            CreateHeader();
             SpacePort spacePort = new SpacePort("SpaceRefuel",1,20,PortStatus.Open);
-            
+
+            Console.WriteLine($"            Welcome to {spacePort.Name}!\n\n");
+            Console.WriteLine("Please enter your information");
+            Console.WriteLine();
+            Console.Write("Name: ");
             var visitorName = Console.ReadLine();
             var visitorArray = await PeopleAPI.ProcessPeople(visitorName);
 
-            var theVisitor = visitorArray.VisitorResult[0];
-            Console.WriteLine(theVisitor.Name);
+            if (visitorArray.VisitorResult.Length != 0)
+            {
 
-            
-            Console.WriteLine($"Welcome to {spacePort.Name}:)\n\n");
+                foreach (var v in visitorArray.VisitorResult)
+                {
+                    if (v.Name.ToLower().Contains(visitorName))
+                    {
+                        var theVisitor = visitorArray.VisitorResult[0];
+                        Console.WriteLine(theVisitor.Name);
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine($"Which ship are you flying today?");
+                        var visitorShip = Console.ReadLine();
+                        var starWars = await StarwarsAPI.ProcessSpaceShips(visitorShip);
 
-            Console.WriteLine($"Which ship are you flying today?");
-            var visitorShip = Console.ReadLine();
-            var starWars = await StarwarsAPI.ProcessSpaceShips(visitorShip);
+                        Console.WriteLine(starWars.Spaceships[0].Name);
+                    }
 
-            Console.WriteLine(starWars.Spaceships[0].Name);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("I'm sorry to inform you that you do not have the required qualifications to enter our SpacePort.");
+                Console.WriteLine();
+            }
+
         }
 
-     
-        
-            
-           
-        
-       
+
+        private static void CreateHeader()
+        {
+            var header = new[]
+           {
+                    @"       ,---.                                  ,------.         ,---.               ,--.           ",
+                    @"      '   .-'  ,---.  ,--,--. ,---. ,---.     |  .--. ' ,---. /  .-',--.,--. ,---. |  |           ",
+                    @"      `.  `-. | .-. |' ,-.  || .--'| .-. :    |  '--'.'| .-. :|  `-,|  ||  || .-. :|  |           ",
+                    @"      .-'    || '-' '\ '-'  |\ `--.\   --.    |  |\  \ \   --.|  .-''  ''  '\   --.|  |           ",
+                    @"      `-----' |  |-'  `--`--' `---' `----'    `--' '--' `----'`--'   `----'  `----'`--'  .         ",
+                    @"              `--'                                                                                "
+
+            };
+
+            Console.WindowWidth = 100;
+            Console.WriteLine("\n\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            foreach (string line in header)
+            {
+                Console.WriteLine(line);
+            }
+
+
+
+
+        }
+
+
+
+
     } 
 }
