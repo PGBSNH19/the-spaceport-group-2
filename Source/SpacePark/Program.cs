3600d47256454268924892ea7ded011f366cd938
Murat <m.kugic@gmail.com>
2020-03-24 15:00:58 +0100

    .

--- Source/SpacePark/Program.cs
+++ Source/SpacePark/Program.cs
@@ -17,139 +17,72 @@ namespace SpacePark
             // Creates a brand new SpacePort Garage...We only need on to start with!
             //SpacePort spacePort = CreateSpacePort(context);
 
-
-            //var availableSpaces = context.ParkingLots.ToList();
-
-            //foreach (var item in availableSpaces)
-            //{
-            //    Console.WriteLine(item.ParkingLotID);
-            //}
-
-            //var spaces = context.ParkingLots
-            //    .Where(p => p.ParkingLotOccupied == false);
-
-            var spaces = context.SpacePorts
-                .Where(p => p.SpacePortID == 1)
-                .Select(s => s.ParkingSpace).ToList();
-
-            var availableSpaces = spaces[0];
-
-            while (availableSpaces > 0)
+            while (true)
             {
-                Console.WriteLine($"Available spaces: { availableSpaces }");
-                Console.WriteLine($"            Welcome to !\n\n");
-                Console.WriteLine("Please enter your information");
-                Console.WriteLine();
-                Console.Write("Name: ");
-                var visitorName = Console.ReadLine();
-                var visitorArray = await PeopleAPI.ProcessPeople(visitorName);
+                Console.WriteLine("Press 1 to park: ");
+                Console.WriteLine("Press 2 to pay: ");
+                var userChouice = Convert.ToInt32(Console.ReadLine());
 
-                // Adding visitor to DB
-                var theVisitor = visitorArray.VisitorResult[0];
-                Visitor visitor = new Visitor
+                if (userChouice == 1)
                 {
-                    Name = theVisitor.Name,
-                    Status = HasPaid.NotPaid
-                };
-                context.Visitors.Add(visitor);
-                context.SaveChanges();
-
-                // Occupying a single parkinglot/space
-                var parkingspace = new ParkingLot
+                    Console.Clear();
+                    await RentParkingSpace(context);
+                }
+                else
                 {
-                    ParkingLotOccupied = true
-                };
-                context.ParkingLots.Add(parkingspace);
-                context.SaveChanges();
-
-                // Bringing it together in VisitorParking to keep track of who parked where
-                var visitorParking = new VisitorParking
-                {
-                    VisitorID = visitor.VisitorID,
-                    ParkingLotID = parkingspace.ParkingLotID
-                };
-                context.VisitorParking.Add(visitorParking);
-                context.SaveChanges();
-
-                availableSpaces -= 1;
+                    Console.Clear();
+                    Console.WriteLine("Paying");
+                }
             }
-            Console.WriteLine($"Available spaces: { availableSpaces }");
+        }
 
+        private static async Task VacateParkingSpace(SpaceParkContext context)
+        {
+            Console.WriteLine("Enter your ");
+            Console.WriteLine();
+            Console.Write("Name: ");
+            var visitorName = Console.ReadLine();
+        }
 
+        private static async Task RentParkingSpace(SpaceParkContext context)
+        {
+            var parkingSpaces = context.ParkingLots.ToList();
 
+            foreach (var parkingSpace in parkingSpaces)
+            {
+                while (parkingSpace.ParkingLotOccupied == false)
+                {
+                    Console.WriteLine($"            Welcome to !\n\n");
+                    Console.WriteLine("Please enter your information");
+                    Console.WriteLine();
+                    Console.Write("Name: ");
+                    var visitorName = Console.ReadLine();
+                    var visitorArray = await PeopleAPI.ProcessPeople(visitorName);
 
+                    // Adding visitor to DB
+                    var theVisitor = visitorArray.VisitorResult[0];
+                    Visitor visitor = new Visitor
+                    {
+                        Name = theVisitor.Name,
+                        Status = HasPaid.NotPaid
+                    };
+                    context.Visitors.Add(visitor);
+                    context.SaveChanges();
 
+                    // Bringing it together in VisitorParking to keep track of who parked where
+                    var visitorParking = new VisitorParking
+                    {
+                        VisitorID = visitor.VisitorID,
+                        ParkingLotID = parkingSpace.ParkingLotID
+                    };
 
+                    // Changing parking space to occupado!
+                    parkingSpace.ParkingLotOccupied = true;
 
-
-
-
-
-
-
-
-            //while (pSpots.ParkingLots.Count <= 5)
-            //{
-            //    if (visitorArray.VisitorResult.Length != 0 || visitorArray.VisitorResult == null)
-            //    {
-
-            //        foreach (var v in visitorArray.VisitorResult)
-            //        {
-            //            if (v.Name.ToLower().Contains(visitorName.ToLower()))
-            //            {
-            //                var theVisitor = visitorArray.VisitorResult[0];
-            //                Console.WriteLine(theVisitor.Name);
-            //                Console.WriteLine();
-
-            //                Console.WriteLine();
-            //                Console.WriteLine($"Which ship are you flying today?");
-            //                var visitorShip = Console.ReadLine();
-            //                var starWars = await StarwarsAPI.ProcessSpaceShips(visitorShip);
-
-            //                Console.WriteLine(starWars.Spaceships[0].Name);
-
-
-            //                Visitor visitor = new Visitor
-            //                {
-            //                    Name = theVisitor.Name,
-            //                    Status = HasPaid.NotPaid,
-            //                };
-
-
-            //                context.Visitors.Add(visitor);
-            //                context.SaveChanges();
-            //                ParkingLot parkingLot = new ParkingLot
-            //                {
-
-            //                    ParkingLotOccupied = true,
-            //                    ParkingLotNO = 1
-            //                };
-
-            //                context.ParkingLots.Add(parkingLot);
-            //                context.SaveChanges();
-
-            //                VisitorParking visitorParking = new VisitorParking
-            //                {
-            //                    ParkingLotID = parkingLot.ParkingLotID,
-            //                    VisitorID = visitor.VisitorID,
-            //                    ParkingNO = 1
-            //                };
-
-            //                context.VisitorParking.Add(visitorParking);
-            //                context.SaveChanges();
-
-            //            }
-
-            //        }
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("I'm sorry to inform you that you do not have the required qualifications to enter our SpacePort.");
-            //        Console.WriteLine();
-            //    }
-
-            //}
-
+                    context.VisitorParking.Add(visitorParking);
+                    context.SaveChanges();
+                }
+            }
         }
 
         private static SpacePort CreateSpacePort(SpaceParkContext context)
