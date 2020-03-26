c676a5d632075fa02d6c012af5a88faf683cc142
Fredrika Eriksson <56867257+erifredrika@users.noreply.github.com>
2020-03-26 10:14:52 +0100

    update
    update

--- Source/SpacePark.Library/Models/ParkingLot.cs
+++ Source/SpacePark.Library/Models/ParkingLot.cs
@@ -7,18 +7,9 @@ namespace SpacePark.Library.Models
     public class ParkingLot
     {
         public int ParkingLotID { get; set; }
-        public bool ParkingLotOccupied { get; set; }
-
-        public int ParkingLotNO { get; set; }
+        public bool ParkingLotOccupied { get; set; }      
+        public int SpacePortID { get; set; }
 
         public ICollection<VisitorParking> VisitorParking { get; set; }
-
-        //public int VisitorID { get; set; }
-        //public Visitor Visitor { get; set; }
-
-        //public ICollection<Visitor> Visitors { get; set; }
-
-        //public int SpacePortID { get; set; }
-        //public SpacePort SpacePort { get; set; }
     }
 }
